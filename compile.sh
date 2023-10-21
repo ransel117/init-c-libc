7423c0f6e5e96092da1713961bfc9170c8027506
ransel117 <ransel117@protonmail.com>
2023-10-21 19:15:58 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,12 @@
+CC="clang"
+CFLAGS="-Wall -Wpedantic -ggdb -std=c11"
+CDEFINES=""
+CINCLUDES=""
+CFILES="main.c"
+CLIBS=""
 
+EXEC="main"
+
+echo "--------COMPILING--------"
+echo "${CC} ${CFLAGS} ${CDEFINES} ${CINCLUDES} ${CFILES} -o ${EXEC} ${CLIBS}"
+${CC} ${CFLAGS} ${CDEFINES} ${CINCLUDES} ${CFILES} -o ${EXEC} ${CLIBS}
